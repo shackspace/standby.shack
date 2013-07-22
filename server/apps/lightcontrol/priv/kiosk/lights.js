f9ed70a11ccfe773e7988117aa21d0bdf0673bf5
Mr. Pi <mrpi@mr-pi.de>
2013-07-22 13:24:28 +0200

    kiosk stuff

---
+++ server/apps/lightcontrol/priv/kiosk/lights.js
@@ -0,0 +1,191 @@
+var lights=new Array();
+lights.push({
+        "id": 123,
+        "minx": 440,
+        "maxx": 480,
+        "miny": 45,
+        "maxy": 130
+});
+lights.push({
+        "id": 119,
+        "minx": 481,
+        "maxx": 510,
+        "miny": 45,
+        "maxy": 130
+});
+lights.push({
+        "id": 115,
+        "minx": 511,
+        "maxx": 565,
+        "miny": 45,
+        "maxy": 130
+});
+lights.push({
+        "id": 112,
+        "minx": 566,
+        "maxx": 645,
+        "miny": 45,
+        "maxy": 220
+});
+lights.push({
+        "id": 102,
+        "minx": 646,
+        "maxx": 720,
+        "miny": 45,
+        "maxy": 130
+});
+lights.push({
+        "id": 100,
+        "minx": 721,
+        "maxx": 780,
+        "miny": 45,
+        "maxy": 130
+});
+lights.push({
+        "id": 124,
+        "minx": 440,
+        "maxx": 480,
+        "miny": 131,
+        "maxy": 200
+});
+lights.push({
+        "id": 120,
+        "minx": 481,
+        "maxx": 510,
+        "miny": 131,
+        "maxy": 200
+});
+lights.push({
+        "id": 116,
+        "minx": 511,
+        "maxx": 565,
+        "miny": 131,
+        "maxy": 200
+});
+lights.push({
+        "id": 103,
+        "minx": 646,
+        "maxx": 720,
+        "miny": 131,
+        "maxy": 200
+});
+lights.push({
+        "id": 101,
+        "minx": 721,
+        "maxx": 780,
+        "miny": 131,
+        "maxy": 200
+});
+lights.push({
+        "id": 125,
+        "minx": 440,
+        "maxx": 480,
+        "miny": 201,
+        "maxy": 280,
+});
+lights.push({
+        "id": 121,
+        "minx": 481,
+        "maxx": 510,
+        "miny": 201,
+        "maxy": 280
+});
+lights.push({
+        "id": 117,
+        "minx": 511,
+        "maxx": 565,
+        "miny": 201,
+        "maxy": 280
+});
+lights.push({
+        "id": 106,
+        "minx": 646,
+        "maxx": 720,
+        "miny": 201,
+        "maxy": 280
+});
+lights.push({
+        "id": 104,
+        "minx": 721,
+        "maxx": 780,
+        "miny": 201,
+        "maxy": 280
+});
+lights.push({
+        "id": 126,
+        "minx": 440,
+        "maxx": 480,
+        "miny": 281,
+        "maxy": 360
+});
+lights.push({
+        "id": 122,
+        "minx": 481,
+        "maxx": 510,
+        "miny": 281,
+        "maxy": 360
+});
+lights.push({
+        "id": 118,
+        "minx": 511,
+        "maxx": 565,
+        "miny": 281,
+        "maxy": 360
+});
+lights.push({
+        "id": 107,
+        "minx": 646,
+        "maxx": 720,
+        "miny": 281,
+        "maxy": 360
+});
+lights.push({
+        "id": 105,
+        "minx": 721,
+        "maxx": 780,
+        "miny": 281,
+        "maxy": 360
+});
+lights.push({
+        "id": 110,
+        "minx": 646,
+        "maxx": 720,
+        "miny": 361,
+        "maxy": 430
+});
+lights.push({
+        "id": 108,
+        "minx": 721,
+        "maxx": 780,
+        "miny": 361,
+        "maxy": 430
+});
+lights.push({
+        "id": 111,
+        "minx": 646,
+        "maxx": 720,
+        "miny": 431,
+        "maxy": 520
+});
+lights.push({
+        "id": 109,
+        "minx": 721,
+        "maxx": 780,
+        "miny": 431,
+        "maxy": 520
+});
+lights.push({
+        "id": 113,
+        "minx": 566,
+        "maxx": 645,
+        "miny": 221,
+        "maxy": 425
+});
+lights.push({
+        "id": 114,
+        "minx": 566,
+        "maxx": 645,
+        "miny": 426,
+        "maxy": 590
+});
+
