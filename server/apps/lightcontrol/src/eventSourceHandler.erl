4173f41f70b2e11b2b3f12d16c8ba323384c54d3
Mr. Pi <mrpi@mr-pi.de>
2013-07-22 11:39:44 +0200

    see TODO.txt

--- server/apps/lightcontrol/src/eventSourceHandler.erl
+++ server/apps/lightcontrol/src/eventSourceHandler.erl
@@ -16,13 +16,14 @@ init(_Transport, Req, []) ->
         Headers = [{<<"content-type">>, <<"text/event-stream">>}],
 	mainServer:addListener(self()),
         {ok, Req2} = cowboy_req:chunked_reply(200, Headers, Req),
-        {loop, Req2, undefined, hibernate}.
+        {loop, Req2, 0, hibernate}.
 
-info({event,_Event}, Req, State) ->
-	io:format("Event: ~p~n",[_Event]),
-	Data = "Da ist was",
-        ok = cowboy_req:chunk(["id: ", Data, "\ndata: ", "Msg", "\n\n"], Req),
-        {loop, Req, State}.
+info({event,{updateLight,LightID,_LightState}}, Req, ID) ->
+	{_,Msg} = jsonHandler:getLight(LightID),
+        ok = cowboy_req:chunk(["id: ", integer_to_list(ID), "\ndata: ", Msg, "\n\n"], Req),
+        {loop, Req, ID+1, hibernate};
+info(_, Req, ID) ->
+	{loop, Req, ID, hibernate}.
 
-terminate(_Reason, _Req, _State) ->
+terminate(_Reason, _Req, _ID) ->
         ok. 
