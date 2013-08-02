b454d870ef98105f1d08be4c0bb188a0d6ee7507
Mr. Pi <mrpi@mr-pi.de>
2013-08-02 15:51:26 +0200

    power monitor

---
+++ client/www/power/main.js
@@ -0,0 +1,104 @@
+/******************************************************
+ * author: Mr. Pi <mrpi@mr-pi.de>
+ * copyright: 2013 Mr. Pi
+ *-----------------------------------------------------
+ * Lightcontroll
+ * ****************************************************/
+
+function getId(x,y) {
+	for(var i in lights) {
+		if(x>=lights[i].minx && x<=lights[i].maxx && y>=lights[i].miny && y<=lights[i].maxy)
+			return lights[i].id;
+	}
+	return -1;
+}
+
+function toggleLight(id) {
+	httpPut('/2/light/'+id,'t');
+}
+
+function mouseClick(e) {
+	var x=-1;
+	var y=-1;
+	if (!e) var e = window.event;
+	if (e.pageX || e.pageY) {
+		x=e.pageX;
+		y=e.pageY;
+	}
+	else if (e.clientX || e.clientY) {
+		x=e.clientX;
+		y=e.clientY;
+	}
+	var id=getId(x,y);
+	if(id>-1)
+		toggleLight(id);
+//	document.getElementById('myhead').innerHTML='X='+x+' Y='+y+' ID='+id;
+}
+
+function httpPut(theUrl,data) {
+	var xmlhttp=new XMLHttpRequest();
+	xmlhttp.open("PUT", theUrl, true);
+	xmlhttp.setRequestHeader('Content-Type', 'text/plain');
+	xmlhttp.send(data);
+}
+
+function httpGet(theUrl) {
+	var xmlhttp=new XMLHttpRequest();
+	xmlhttp.open("GET", theUrl, false);
+	xmlhttp.send(); 
+	return xmlhttp.response;
+}
+
+function updateLight(id, state) {
+	if(state == 0)
+		document.getElementById('l'+id).style.color='#ff0000';
+	else
+		document.getElementById('l'+id).style.color='#00ff00';
+}
+
+function clearSelection() {
+	if ( document.selection ) {
+		document.selection.empty();
+	} else if ( window.getSelection ) {
+		window.getSelection().removeAllRanges();
+	} else {
+		alert('error');
+	}
+}
+
+function setupEventSource() {
+	var source = new EventSource('/eventsource');
+
+	source.addEventListener('message', function(event) {
+		var obj = JSON.parse(event.data);
+		if(obj.type === "states") {
+			updateLight(obj.id,obj.state);
+		}
+		else if(obj.type === "power") {
+			updateChart(obj.p1,obj.p2,obj.p3);
+		}
+		}, false);
+}
+
+window.onload = function() {
+	for(var i=100; i<=126; i++) {
+		var res = httpGet('/2/light/'+i);
+		var obj = JSON.parse(res);
+		if(obj.state=="0")
+			document.getElementById('l'+i).style.color='#f00';
+		else
+			document.getElementById('l'+i).style.color='#0f0';
+	}
+	self.setInterval(function() {
+		var cd = new Date();
+		document.getElementById('myhead').innerHTML=cd.getHours()+':'+cd.getMinutes()+':'+cd.getSeconds();
+		clearSelection();
+	}, 1000);
+	
+	if (!!window.EventSource) {
+		setupEventSource();
+	} else {
+		alert("Sorry but your browser doesn't support the EventSource API");
+	}
+	chartinit();
+}
---
+++ client/www/power/power.js
@@ -0,0 +1,110 @@
+dataP1 = [];
+dataP2 = [];
+dataP3 = [];
+
+function chartinit() {
+	energieChart = new CanvasJS.Chart("energie",{
+		zoomEnabled: true,
+		panEnabled: true,
+		toolTip: {
+			shared: true,
+		},
+		title: {
+			text: "energie usage:",
+			horizontalAlign: "left",
+			fontSize: 14,
+			fontWeight: "bold",
+			fontFamily: "calibri"
+		},
+		legend: {
+			horizontalAlign: "left",
+			fontSize: 14,
+			fontWeight: "bold",
+			fontFamily: "calibri",
+			fontColor: "dimGrey",
+			valueFormatString: "##0 000"
+		},
+		axisY2:{
+			suffix: 'W',
+			includeZero: false,
+			valueFormatString: "##0 000"
+		},
+		axisX:{      
+			valueFormatString: "HH:mm:ss"
+		},
+		data: [
+			{ 
+				type: "line",
+				axisYType: "secondary",
+				xValueType: "dateTime",
+				showInLegend: true,
+				name: "P1",
+				dataPoints: dataP1
+			}, {				
+				type: "line",
+				axisYType: "secondary",
+				xValueType: "dateTime",
+				showInLegend: true,
+				name: "P2" ,
+				dataPoints: dataP2
+			}, {				
+				type: "line",
+				axisYType: "secondary",
+				xValueType: "dateTime",
+				showInLegend: true,
+				name: "P3" ,
+				dataPoints: dataP3
+			}
+		]
+	});
+
+	var time = new Date();
+	time.setTime(time.getTime()-1000*300);
+
+	for (var i = 0; i < 300; i++) {
+		time.setTime(time.getTime()+1000);
+		dataP1.push({
+			x: time.getTime(),
+			y: 1000
+		});
+		dataP2.push({
+			x: time.getTime(),
+			y: 1000
+		});
+		dataP3.push({
+			x: time.getTime(),
+			y: 1000
+		});
+	};
+
+	energieChart.options.data[0].legendText = " P1: " + -1 + "W ";
+	energieChart.options.data[1].legendText = " P2: " + -1 + "W ";
+	energieChart.options.data[2].legendText = " P3: " + -1 + "W ";
+
+	energieChart.render();
+}
+function updateChart(p1,p2,p3) {
+	var time = new Date();
+	
+	dataP1.push({
+		x: time.getTime(),
+		y: p1
+	});
+	dataP1.shift();
+	dataP2.push({
+		x: time.getTime(),
+		y: p2
+	});
+	dataP2.shift();
+	dataP3.push({
+		x: time.getTime(),
+		y: p3
+	});
+	dataP3.shift();
+
+	energieChart.options.data[0].legendText = " P1: " + p1 + "W ";
+	energieChart.options.data[1].legendText = " P2: " + p2 + "W ";
+	energieChart.options.data[2].legendText = " P3: " + p3 + "W ";
+
+	energieChart.render();
+}
